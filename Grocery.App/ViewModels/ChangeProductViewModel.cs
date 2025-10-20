fa6f672d0ec34152d2f9118b3add66494030ff74
Runar <s1208862@student.windesheim.nl>
2025-10-20 15:09:23 +0200

    Created ChangeProductView and viewmodel.

--- Grocery.App/ViewModels/ChangeProductViewModel.cs
+++ Grocery.App/ViewModels/ChangeProductViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Grocery.App.Views;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using Grocery.Core.Services;
+using Grocery.App.Views;
+using System;
+using System.Collections.ObjectModel;
 using System.Globalization;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Grocery.App.ViewModels
 {
-    public partial class NewProductViewModel : BaseViewModel
+    public partial class ChangeProductViewModel : BaseViewModel
     {
         private readonly IProductService _productService;
-        
+
+        [ObservableProperty]
+        Product selectedProduct;
+
         [ObservableProperty]
         private string? newName;
 
@@ -21,10 +26,10 @@ namespace Grocery.App.ViewModels
         private DateOnly NewShelfLife { get; set; }
 
         [ObservableProperty]
-        private int newShelfLifeYear; 
+        private int newShelfLifeYear;
 
         [ObservableProperty]
-        private int newShelfLifeMonth; 
+        private int newShelfLifeMonth;
 
         [ObservableProperty]
         private int newShelfLifeDay;
@@ -35,9 +40,24 @@ namespace Grocery.App.ViewModels
         [ObservableProperty]
         private string? message;
 
-        public NewProductViewModel(IProductService productService)
+        //public ObservableCollection<BoughtProducts> BoughtProductsList { get; set; } = [];
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ChangeProductViewModel(IProductService productService) 
         {
             _productService = productService;
+            Products = new(productService.GetAll());
+        }
+
+        partial void OnSelectedProductChanged(Product? oldValue, Product newValue)
+        {
+            //BoughtProductsList.Clear();
+            //List<BoughtProducts> list = _boughtProductsService.Get(newValue.Id);
+            //foreach (var item in list)
+            //{
+            //    BoughtProductsList.Add(item);
+            //}
         }
 
         [RelayCommand]
@@ -64,9 +84,6 @@ namespace Grocery.App.ViewModels
             {
                 Message = "De ingevulde gegevens zijn niet geldig, probeer het aub opnieuw.";
             }
-            
-            
-
         }
 
         private async Task GoToProducts()
@@ -74,6 +91,10 @@ namespace Grocery.App.ViewModels
             await Shell.Current.GoToAsync(nameof(ProductView));
         }
 
-
+        [RelayCommand]
+        public void NewSelectedProduct(Product product)
+        {
+            SelectedProduct = product;
+        }
     }
 }
